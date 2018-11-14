561c0bdc45c38d5a5e8750cf1b8133e9e560775e
Nicolejeanne <nicolejeanne@users.noreply.github.com>
2018-11-14 05:38:30 -0600

    added basic content framework to htmlRoutes.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,29 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads index.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/anotherpage", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/anotherpage.html"));
+  });
+
+  // blog route loads blog.html
+  app.get("/yetanotherpage", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/yetanotherpage.html"));
+  });
+
+};

4c462db9b9e8bd569f963fb4f9ce17c224d925e4
Nicolejeanne <nicolejeanne@users.noreply.github.com>
2018-11-18 22:24:42 -0600

    added boolean default to completed column, added seeds minus addresses

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE user (
   id INT NOT NULL AUTO_INCREMENT,
   fullname VARCHAR(100) NOT NULL,
   age TINYINT,
-  gender TINYINT,
+  gender VARCHAR(25),
   googleID VARCHAR(100) NOT NULL,
   emailAddress VARCHAR(255),
   profilePhoto VARCHAR(255),
@@ -15,10 +15,10 @@ CREATE TABLE user (
 
 CREATE TABLE walk (
     id INT NOT NULL AUTO_INCREMENT,
-    walkerId INT,
+    requesterId INT,
     volunteerId INT,
     startLocation VARCHAR(255) NOT NULL,
     endLocation VARCHAR(255) NOT NULL,
     startTime DATETIME
-    completed BOOLEAN NOT NULL
+    completed BOOLEAN DEFAULT (false) NOT NULL
 )

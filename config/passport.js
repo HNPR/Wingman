c078ba54cd130ef68c0fa2eafd8780113297e96a
Ping <pingis@gmail.com>
2018-11-20 19:16:32 -0600

    adding keys and passport files

---
+++ config/passport.js
@@ -0,0 +1,22 @@
+let keys = require("./keys")
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+module.exports = (passport) => {
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
+    passport.use(new GoogleStrategy({
+            clientID: keys.google.id,
+            clientSecret: keys.google.secret,
+            callbackURL: "/auth/google/callback"
+        },
+        (token, refreshToken, profile, done) => {
+            return done(null, {
+                profile: profile,
+                token: token
+            });
+        }));
+};

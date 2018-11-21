021e46ecaf42e231165cc0465105832077a79d27
Ping <pingis@gmail.com>
2018-11-20 19:09:59 -0600

    google OAuth working

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
+// Set up enviornment variables
+require("dotenv").config();
+let keys = require("./config/keys")
 
 // Sets up the Express App
 // =============================================================
+var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+// Set up PassportJS for OAuth
+const passport = require('passport');
+const auth = require('./config/passport');
+auth(passport);
+app.use(passport.initialize());
+
+// Set up cookies
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: [keys.cookie.key],
+  maxAge: 24 * 60 * 60 * 1000
+}));
+app.use(cookieParser());
 
 // Requiring our models for syncing
 var db = require("./models");

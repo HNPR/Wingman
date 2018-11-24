a72fca220708ee9c88af06d1859e5823c05589f8
Ping <pingis@gmail.com>
2018-11-24 02:10:35 -0600

    fixed api to get walks

--- routes/walk-apiRoutes.js
+++ routes/walk-apiRoutes.js
@@ -1,43 +1,43 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-// GET route for getting all of the requested walks
-app.get("/api/walks", function(req, res) {
+  // GET route for getting all of the requested walks that are incomplete
+  app.get("/api/walks", function (req, res) {
     db.Walk.findAll({
       where: {
-        // completed: is false => Ping! I do not know how to accomplish this
+        completed: false
       }
-    }).then(function(dbWalk) {
+    }).then(function (dbWalk) {
       res.json(dbWalk);
     });
   });
 
   // Get route for retrieving a single user's requested walks
-  app.get("/api/walks/:id", function(req, res) {
+  app.get("/api/walks/:requesterID", function (req, res) {
     db.Walk.findAll({
       where: {
-        id: req.params.id
+        requesterID: req.params.requesterID
       }
-    }).then(function(dbWalk) {
+    }).then(function (dbWalk) {
       res.json(dbWalk);
     });
   });
 
   // POST route for saving a new walk
-  app.post("/api/walks", function(req, res) {
-    db.Walk.create(req.body).then(function(dbWalk) {
+  app.post("/api/walks", function (req, res) {
+    db.Walk.create(req.body).then(function (dbWalk) {
       res.json(dbWalk);
     });
   });
 
   // DELETE route for deleting walks
-  app.delete("/api/walks/:id", function(req, res) {
+  app.delete("/api/walks/:id", function (req, res) {
     db.Walk.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbWalk) {
+    }).then(function (dbWalk) {
       res.json(dbWalk);
     });
   });
